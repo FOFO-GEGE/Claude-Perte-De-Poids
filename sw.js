6f13781b91d1c9d2b7f4737a3ee4c5c905ec1f04
FOFO-GEGE <minkomarcus1997@gmail.com>
2026-05-19 13:49:01 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'poids-v1';
+const SHELL = ['./', './index.html', './manifest.json', './icons/icon.svg'];
+const OFF_HOST = 'world.openfoodfacts.org';
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (new URL(e.request.url).hostname === OFF_HOST) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200) {
+          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
